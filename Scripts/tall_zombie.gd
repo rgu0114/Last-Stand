bedb9a9a8c1a34a23be3f8460a823d955a0e1664
Richard Gu <richardgu0114@gmail.com>
2025-09-04 22:57:45 -0700

    working run and attack animations

--- Scripts/tall_zombie.gd
+++ Scripts/tall_zombie.gd
@@ -1,33 +1,61 @@
 extends CharacterBody3D
 
 var player = null
+var state_machine
 
 const SPEED = 4.0
+const ATTACK_RANGE = 2.0
 
 @export var player_path : NodePath
 
 @onready var nav_agent = $NavigationAgent3D
+@onready var anim_tree = $AnimationTree
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Searching for players...")
 	var players = get_tree().get_nodes_in_group("players")
-	print("Found ", players.size(), " players")
 	if players.size() > 0:
 		player = players[0]
 		print("Player found: ", player.name)
 	else:
 		print("No player found in 'players' group")
-		# Debug: print all available groups
-		print("Available groups: ", get_tree().get_nodes_in_group(""))
+		
+	state_machine = anim_tree.get("parameters/playback")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if player == null:
 		print("Player is null!")
 		return
+	
+	if state_machine == null:
+		print("State Machine is null!")
+		return
+	
 	velocity = Vector3.ZERO
-	nav_agent.set_target_position(player.global_transform.origin)
-	var next_nav_point = nav_agent.get_next_path_position()
-	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+	
+	var current_state = state_machine.get_current_node()
+	
+	match current_state:
+		"zombie_run":
+			# Navigation
+			nav_agent.set_target_position(player.global_transform.origin)
+			var next_nav_point = nav_agent.get_next_path_position()
+			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			look_at(Vector3(global_position.x + velocity.x, global_position.y, global_position.z + velocity.z), Vector3.UP)
+		"zombie_attack":
+			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
+	# Conditions
+	anim_tree.set("parameters/conditions/attack", _target_in_range())
+	anim_tree.set("parameters/conditions/run", !_target_in_range())
+	
+	#anim_tree.get("parameters/playback")
+	
 	move_and_slide()
+	
+func _target_in_range():
+	return global_position.distance_to(player.global_position) < ATTACK_RANGE
+
+func _hit_finished():
+	player.hit()
